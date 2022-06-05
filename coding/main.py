fce2290aba0639230e140aed1d8192d9fddb9958
Marco Tröster <marco@troester-gmbh.de>
2022-06-05 21:03:20 +0200

    restructure main, use dependency injection

--- coding/main.py
+++ coding/main.py
@@ -1,10 +1,15 @@
+from .settings import twitch_settings
+from .tft_remote_control import TFTRemoteControl
+from .twitch_connection import TwitchConnection
 from .tft_overlay_ui import TFTRemoteControlOverlayUI
-from .twitch_script import TwitchBot
+from .twitch_script import TFTTwitchBot
 
 
 def main():
-    chatbot = TwitchBot()
-    overlay_ui = TFTRemoteControlOverlayUI(chatbot.start_bot, chatbot.stop)
+    tft_remote = TFTRemoteControl()
+    twitch_connection = TwitchConnection(twitch_settings())
+    twitch_chatbot = TFTTwitchBot(twitch_connection, tft_remote)
+    overlay_ui = TFTRemoteControlOverlayUI(twitch_chatbot.start_bot, twitch_chatbot.stop_bot)
     overlay_ui.display_as_daemon()
 
 
