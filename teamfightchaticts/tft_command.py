e076310f479f6660e797b4b1ccb8b27214314621
Marco Tröster <marco@troester-gmbh.de>
2022-06-07 16:25:59 +0200

    transform properties into dataclass attributes -> dataclass repr shows the properties now

--- teamfightchaticts/tft_command.py
+++ teamfightchaticts/tft_command.py
@@ -22,7 +22,7 @@ class TFTCmdType(IntEnum):
 @dataclass(eq=True)
 class TFTCommand:
     cmd: str
-    cmd_patterns: Dict[TFTCmdType, str]=field(hash=False, compare=False,
+    cmd_patterns: Dict[TFTCmdType, str]=field(hash=False, compare=False, repr=False,
         default_factory=lambda: {
             TFTCmdType.SHOP: '^shop[1-5]$',
             TFTCmdType.PICK_AUGMENT: '^aug[1-3]$',
@@ -36,44 +36,39 @@ class TFTCommand:
             TFTCmdType.COLLECT_ITEMS_OF_ROW: '^row[1-8]$',
             TFTCmdType.ATTACH_ITEM: '^[a-j]w[0-9]$',
         })
+    cmd_type: TFTCmdType=field(init=False, default=None)
+    selected_shop_unit: int=field(init=False, default=None)
+    selected_augment: int=field(init=False, default=None)
+    unit_to_sell: str=field(init=False, default=None)
+    unit_to_place: str=field(init=False, default=None)
+    unit_place_aim: str=field(init=False, default=None)
+    row_to_collect: str=field(init=False, default=None)
+    item_to_atttach: str=field(init=False, default=None)
+    unit_to_attach_to: str=field(init=False, default=None)
 
-    @property
-    def cmd_type(self) -> TFTCmdType:
-        for cmd_type in self.cmd_patterns:
-            if re.match(self.cmd_patterns[cmd_type], self.cmd):
-                return cmd_type
-        return TFTCmdType.INVALID
+    def __post_init__(self):
+        # determine command type
+        self.cmd = self.cmd.lower()
+        self.cmd_type = TFTCmdType.INVALID
+        for cmd_type_check in self.cmd_patterns:
+            if re.match(self.cmd_patterns[cmd_type_check], self.cmd):
+                self.cmd_type = cmd_type_check
+                break
 
-    @property
-    def selected_shop_unit(self) -> int:
-        return int(self.cmd[4]) - 1
-
-    @property
-    def selected_augment(self) -> int:
-        return int(self.cmd[3]) - 1
-
-    @property
-    def unit_to_sell(self) -> str:
-        return self.cmd[4:6]
-
-    @property
-    def unit_to_place(self) -> str:
-        return self.cmd[0:2]
-
-    @property
-    def unit_place_aim(self) -> str:
-        return self.cmd[2:4]
-
-    @property
-    def row_to_collect(self) -> str:
-        return int(self.cmd[3:])
-
-    @property
-    def item_to_atttach(self) -> int:
-        slot_char = self.cmd[0].encode()[0]
-        index = slot_char - "a".encode()[0]
-        return index
-
-    @property
-    def unit_to_attach_to(self) -> str:
-        return self.cmd[1:]
+        # parse type-specific command parameters
+        if self.cmd_type == TFTCmdType.SHOP:
+            self.selected_shop_unit = int(self.cmd[4]) - 1
+        if self.cmd_type == TFTCmdType.PICK_AUGMENT:
+            self.selected_augment = int(self.cmd[3]) - 1
+        if self.cmd_type == TFTCmdType.SELL_UNIT:
+            self.unit_to_sell = self.cmd[4:6]
+        if self.cmd_type == TFTCmdType.PLACE_UNIT:
+            self.unit_to_place = self.cmd[0:2]
+            self.unit_place_aim = self.cmd[2:4]
+            if self.unit_to_place == self.unit_place_aim:
+                self.cmd_type = TFTCmdType.INVALID
+        if self.cmd_type == TFTCmdType.COLLECT_ITEMS_OF_ROW:
+            self.row_to_collect = int(self.cmd[3:])
+        if self.cmd_type == TFTCmdType.ATTACH_ITEM:
+            self.item_to_atttach = self.cmd[0].encode()[0] - "a".encode()[0]
+            self.unit_to_attach_to = self.cmd[1:]
