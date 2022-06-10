1888c5dd64c6c4d5b7c6399fbbb42f5f881ebd33
Marco Tröster <marco@troester-gmbh.de>
2022-06-10 03:32:08 +0200

    add connection test stubs to be implemented

---
+++ tests/test_chatbot.py
@@ -0,0 +1,21 @@
+from teamfightchaticts.twitch_connection import TwitchConnection
+
+
+def test_should_connect_to_chat():
+    pass
+
+
+def test_should_disconnect_from_chat_gracefully():
+    pass
+
+
+def test_should_send_chat_pong():
+    pass
+
+
+def test_should_parse_tft_commands_from_chat_with_single_recv():
+    pass
+
+
+def test_should_parse_tft_commands_from_chat_replay_with_overlapping_recv_buffes():
+    pass
